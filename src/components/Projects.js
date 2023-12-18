7264a7676eafba81e1fadc20bb02452a689929e3
Gurpreet <gurpreetd0824@gmail.com>
2023-12-17 19:28:15 -0700

    removed react fragment, not needed

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,79 +6,77 @@ import numpyLogo from "../logos/numpy-1.svg";
 
 function Projects() {
   return (
-    <>
-      <div className="container scoll-appear-animation" id="projects">
-        <div className="d-flex justify-content-center">
-          <h1 className="text">Projects:</h1>
-        </div>
-        <div className="d-flex mb-3" id="projects-layout">
-          <div className="p-2">
-            <a href="">
-              <div className="card">
-                <div className="card-body">
-                  <h5 className="card-title">Raspberry Pi Drone</h5>
-                  <p className="card-text">
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
-                  </p>
-                  <hr />
-                  <div className="d-flex flex-wrap justify-content-center">
-                    <div className="p-2 bd-highlight">
-                      {" "}
-                      <img src={rpi} style={{ maxWidth: 20 + "px" }} />
-                    </div>
-                    <div className="p-2 bd-highlight">
-                      <img src={py} style={{ maxWidth: 20 + "px" }} />
-                    </div>
-                    <div className="p-2 bd-highlight">
-                      <img src={redis} style={{ maxWidth: 20 + "px" }} />
-                    </div>
-                    <div className="p-2 bd-highlight">
-                      <img src={reactLogo} style={{ maxWidth: 20 + "px" }} />
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </a>
-          </div>
-          <div className="p-2">
+    <div className="container scoll-appear-animation" id="projects">
+      <div className="d-flex justify-content-center">
+        <h1 className="text">Projects:</h1>
+      </div>
+      <div className="d-flex mb-3" id="projects-layout">
+        <div className="p-2">
+          <a href="">
             <div className="card">
               <div className="card-body">
-                <h5 className="card-title">Sun Position Tracking</h5>
+                <h5 className="card-title">Raspberry Pi Drone</h5>
                 <p className="card-text">
                   Some quick example text to build on the card title and make up
                   the bulk of the card's content.
                 </p>
                 <hr />
                 <div className="d-flex flex-wrap justify-content-center">
-                  {" "}
                   <div className="p-2 bd-highlight">
+                    {" "}
                     <img src={rpi} style={{ maxWidth: 20 + "px" }} />
                   </div>
                   <div className="p-2 bd-highlight">
                     <img src={py} style={{ maxWidth: 20 + "px" }} />
                   </div>
                   <div className="p-2 bd-highlight">
-                    <img src={numpyLogo} style={{ maxWidth: 20 + "px" }} />
+                    <img src={redis} style={{ maxWidth: 20 + "px" }} />
+                  </div>
+                  <div className="p-2 bd-highlight">
+                    <img src={reactLogo} style={{ maxWidth: 20 + "px" }} />
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div className="p-2">
-            <div className="card">
-              <div className="card-body">
-                <h5 className="card-title">Farming IoT</h5>
-                <p className="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
+          </a>
+        </div>
+        <div className="p-2">
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title">Sun Position Tracking</h5>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <hr />
+              <div className="d-flex flex-wrap justify-content-center">
+                {" "}
+                <div className="p-2 bd-highlight">
+                  <img src={rpi} style={{ maxWidth: 20 + "px" }} />
+                </div>
+                <div className="p-2 bd-highlight">
+                  <img src={py} style={{ maxWidth: 20 + "px" }} />
+                </div>
+                <div className="p-2 bd-highlight">
+                  <img src={numpyLogo} style={{ maxWidth: 20 + "px" }} />
+                </div>
               </div>
             </div>
           </div>
         </div>
+        <div className="p-2">
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title">Farming IoT</h5>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
-    </>
+    </div>
   );
 }
 
