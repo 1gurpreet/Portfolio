867ea4338bf5fbef5da6db456c1bfd9f308fabb4
Gurpreet <gurpreetd0824@gmail.com>
2023-12-19 21:31:12 -0700

    minor changes

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,6 +3,7 @@ import py from "../logos/python-5.svg";
 import redis from "../logos/redis.svg";
 import reactLogo from "../logos/react-2.svg";
 import numpyLogo from "../logos/numpy-1.svg";
+import microPythonLogo from "../logos/Micropython-logo.svg.png"
 
 function Projects() {
   return (
@@ -17,33 +18,7 @@ function Projects() {
         className="d-flex flex-row flex-wrap justify-content-center mb-3"
         id="projects-layout"
       >
-        <div className="p-2">
-          <div className="card">
-            <div className="card-body">
-              <h5 className="card-title">Raspberry Pi Drone</h5>
-              <p className="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
-              <hr />
-              <div className="d-flex flex-wrap justify-content-center">
-                <div className="p-2 bd-highlight">
-                  {" "}
-                  <img src={rpi} style={{ maxWidth: 20 + "px" }} />
-                </div>
-                <div className="p-2 bd-highlight">
-                  <img src={py} style={{ maxWidth: 20 + "px" }} />
-                </div>
-                <div className="p-2 bd-highlight">
-                  <img src={redis} style={{ maxWidth: 20 + "px" }} />
-                </div>
-                <div className="p-2 bd-highlight">
-                  <img src={reactLogo} style={{ maxWidth: 20 + "px" }} />
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+
         <div className="p-2">
           <div className="card">
             <div className="card-body">
@@ -76,6 +51,24 @@ function Projects() {
                 Some quick example text to build on the card title and make up
                 the bulk of the card's content.
               </p>
+              <hr />
+              <div className="p-2 bd-highlight">
+                  <img src={microPythonLogo} style={{ maxWidth: 30 + "px" }} />
+                </div>
+            </div>
+          </div>
+        </div>
+        <div className="p-2">
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title">Maze Visualizer</h5>
+              <p className="card-text">
+                Data Structures and algorithms. 
+              </p>
+              <hr />
+              <div className="p-2 bd-highlight">
+                  <img src={reactLogo} style={{ maxWidth: 20 + "px" }} />
+                </div>
             </div>
           </div>
         </div>
